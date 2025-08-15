7658a99fd8cabf153e3c8179cbb84b14ed7678cc
geril <gergert.ilya@mail.ru>
2025-08-15 09:42:35 +0300

    learn-haskell: recursion prelude

--- learn-haskell/hello.hs
+++ learn-haskell/hello.hs
@@ -1,4 +1,5 @@
 import Html
+import Prelude hiding (even, odd, replicate)
 
 main :: IO ()
 main = putStrLn (render myHtml)
@@ -9,3 +10,17 @@ myHtml =
     ( ul_
         [p_ "123", p_ "321", h1_ "123"]
     )
+
+replicate_ :: Int -> a -> [a]
+replicate_ num a =
+  if num <= 0
+    then []
+    else a : replicate_ (num - 1) a
+
+-- even :: Int -> Bool
+-- even a = if a == 0 then True else odd (a - 1)
+
+-- even a = a == 0 || odd (a - 1)
+
+-- odd :: Int -> Bool
+-- odd a = a == 1 || even (a - 1)
